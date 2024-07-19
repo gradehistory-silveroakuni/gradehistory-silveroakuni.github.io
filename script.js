33747ab70dba54867228a0bcfcfe81a7c2f8c015
gradehistory-silveroakuni <156331118+gradehistory-silveroakuni@users.noreply.github.com>
2024-07-19 21:03:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkLogin() {
     // Replace these values with your actual username and password
-    var providedUsername = "2201030600001";
-    var providedPassword = "Aindrik@123";
+    var providedUsername = "2201030600015";
+    var providedPassword = "Naman@123";
 
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
